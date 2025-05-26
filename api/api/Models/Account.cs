18e1f8fde522a012043b6bca3cda9a28415976ab
luisacuna21 <acunaluis560@gmail.com>
2025-05-26 12:33:44 -0600

    feat: add AnnualInterestRate to Account
    Also add migrations.

--- api/api/Models/Account.cs
+++ api/api/Models/Account.cs
@@ -5,11 +5,22 @@ namespace api.Models
     public class Account
     {
         public int Id { get; set; }
+        
         [StringLength(19, ErrorMessage = "El número de cuenta no puede exceder los 20 caracteres")]
         public string AccountNumber { get; set; }
+        
         public decimal InitialBalance { get; set; }
+
+        /// <summary>
+        /// The annual interest rate for the account, expressed as a percentage.
+        /// For example: 5.0 for 5% interest rate.
+        /// </summary>
+        public decimal AnnualInterestRate { get; set; }
+
         public int CustomerId { get; set; }
+        
         public Customer Customer { get; set; }
+        
         public DateTime CreatedAt { get; set; } = DateTime.Now;
 
         public List<Transaction> Transactions { get; set; }
